4de65767b6051ec6d5afe91ce5255fcdf1cf4b43
Amanda <amandadattalo@gmail.com>
2024-06-02 20:13:30 -0700

    Drop files from .gitignore

---
+++ Finance Tracker/Finance Tracker/Data/Finance_TrackerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Finance_Tracker.Models;
+
+namespace Finance_Tracker.Data
+{
+    public class Finance_TrackerContext : DbContext
+    {
+        public Finance_TrackerContext (DbContextOptions<Finance_TrackerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Finance_Tracker.Models.Account> Account { get; set; } = default!;
+    }
+}
---
+++ Finance Tracker/Finance Tracker/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace Finance_Tracker.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required float Value { get; set; }
+    }
+}
